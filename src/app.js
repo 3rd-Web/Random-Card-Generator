ba18de5ba3d5372f2c5e30c5318c44610fee7943
3rd-Web <web_3@icloud.com>
2022-06-15 15:46:56 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function randomNumber() {
+  let arrNum = [
+    "ꋫ",
+    "𝟚",
+    "𝟛",
+    "𝟜",
+    "𝟝",
+    "𝟞",
+    "𝟟",
+    "𝟠",
+    "𝟡",
+    "𝟙𝟘",
+    "ꀭ",
+    "ℚ",
+    "ꀗ"
+  ];
+  let randomIndex = Math.floor(Math.random() * arrNum.length);
+  return arrNum[randomIndex];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function randomSuit() {
+  let arrSuit = ["&#9830", "&#9829", "&#9824", "&#9827"];
+  let randomSuit = Math.floor(Math.random() * arrSuit.length);
+  if (randomSuit == 0 || randomSuit == 1) {
+    document.querySelector(".card-container").classList.add("red");
+  } else {
+    document.querySelector(".card-container").classList.add("black");
+  }
+  return arrSuit[randomSuit];
+}
+
+let suitValue = randomSuit();
+
+window.onload = () => {
+  document.querySelector("#generate").innerHTML = randomNumber();
+  document.querySelector(".top").innerHTML = suitValue;
+  document.querySelector(".bottom").innerHTML = suitValue;
 };
